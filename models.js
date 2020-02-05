f861049fe197b67e9fa6e7df695d4576f226a4f3
Maurício Rodrigues Batista <mbatista@grupoa.com.br>
2020-02-05 09:36:37 -0300

    Implemented Custom Response in models lib and core lib

--- models.js
+++ models.js
@@ -1,5 +1,7 @@
 const customError    = require('./models/CustomError.model');
+const customResponse = require('./models/CustomResponse.model');
 
 module.exports = {
-  customError
+  customError,
+  customResponse
 };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
+const bodyValidator  =  require('./validator/body.validator');
 const customError    = require('./models/CustomError.model');
+const customResponse = require('./models/CustomResponse.model');
 const dbPgService    = require('./services/dbPg.service');
 const logger         = require('./logger');
 const requestHttp    = require('./http/request.http');
 const responseHttp   = require('./http/response.http');
-const bodyValidator  =  require('./validator/body.validator');
 const xrayConfig     =  require('./configs/xray.config');
 
 module.exports = {
+  bodyValidator,
   customError,
+  customResponse,
   dbPgService,
   logger,
   requestHttp,
   responseHttp,
-  bodyValidator,
   xrayConfig
 };
